a88983f4e2c4520d03036d12daa2f10f9504a16e
Riyad <Riyad9876@gmail.com>
2023-08-24 10:31:49 -0400

    res.status.json instead of res.send

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const plants = ['Monstera Deliciosa', 'Corpse Flower', 'Elephant-Foot Yam', "Witches' Butter",];
+
+
 app.get('/', (req, res) =>{
     res.send('hello')
 })
 
+
+app.get('/plants', (req, res) => {
+    // res.send(plants)
+    res.status(200).json({products : plants , category: 'Botany'})
+})
+
+
+
+
 app.listen(PORT, ()=>{
     console.log('Listening on ', PORT)
 })
+
+
